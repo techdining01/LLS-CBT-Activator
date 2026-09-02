9f3476b9b11a8e6c17e9763cb1e68a7b353e9981
techdining01 <amooade12@gmail.com>
2026-09-02 22:45:44 +0100

    db re-initialization

---
+++ create_tables.py
@@ -0,0 +1,22 @@
+
+
+from app.database.database import engine, Base
+
+# IMPORTANT:
+# Import models so SQLAlchemy knows about all tables.
+from app.database.models import (
+    Subject,
+    Question,
+    Option,
+    QuestionImage,
+    ExamSession,
+    ExamSubject,
+    ExamQuestion,
+    StudentAnswer,
+    ProductLicense,
+    LicenseActivation,
+)
+
+Base.metadata.create_all(bind=engine)
+
+print("Database tables created successfully.")
